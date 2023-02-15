2730047b84bbd9849ec2fadb4350c3ac58bb098f
florencialecha <flecharegueira@gmail.com>
2023-02-15 20:06:57 -0300

    Agregar paquete entity + clase Category

---
+++ backend/src/main/java/com/digitalbooking/backend/entity/Category.java
@@ -0,0 +1,20 @@
+package com.digitalbooking.backend.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "categories")
+
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private String imageUrl;
+
+    public Category() {
+    }
+
+}
