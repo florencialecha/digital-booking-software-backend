de4db076c7f8a53c0dc230cc91a2d5d2b9d5c8c4
florencialecha <flecharegueira@gmail.com>
2023-02-15 20:55:16 -0300

    Fix cateogry repository

--- backend/src/main/java/com/digitalbooking/backend/repository/CategoryRepository.java
+++ backend/src/main/java/com/digitalbooking/backend/repository/CategoryRepository.java
@@ -2,9 +2,11 @@ package com.digitalbooking.backend.repository;
 
 import com.digitalbooking.backend.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-public class CategoryRepository extends JpaRepository<Category, Long> {
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
 
 }
