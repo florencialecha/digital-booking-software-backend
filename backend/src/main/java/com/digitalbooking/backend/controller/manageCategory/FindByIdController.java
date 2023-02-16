ae637743d3748cc731480d8fa041cfca476beb32
florencialecha <flecharegueira@gmail.com>
2023-02-16 19:36:53 -0300

    Cree nuevo proyecto

--- backend/src/main/java/com/digitalbooking/backend/controller/manageCategory/FindByIdController.java
+++ backend/src/main/java/com/digitalbooking/backend/controller/manageCategory/FindByIdController.java
@@ -4,7 +4,9 @@ import com.digitalbooking.backend.entity.Category;
 import com.digitalbooking.backend.exception.ResourceNotFoundException;
 import com.digitalbooking.backend.service.CategoryService;
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,17 +14,19 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("findCategory")
+@RequestMapping("/findCategory")
 
 public class FindByIdController {
 
     private CategoryService categoryService;
     private final Logger LOGGER = Logger.getLogger(FindByIdController.class);
 
+    @Autowired
     public FindByIdController(CategoryService categoryService) {
         this.categoryService = categoryService;
     }
 
+    @GetMapping("/byId")
     public ResponseEntity<Optional<Category>> findById(@RequestParam Long id) throws ResourceNotFoundException {
         LOGGER.info("Request send: you are trying to find a Category.");
         Optional<Category> searchCategory = categoryService.findBy(id);
