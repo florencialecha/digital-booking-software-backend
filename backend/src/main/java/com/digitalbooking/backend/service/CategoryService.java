80d8f2cf369ca30fdfa27b4795f97c2b722ea2b4
florencialecha <flecharegueira@gmail.com>
2023-02-16 15:54:55 -0300

    Agregar controlador findbyid category

--- backend/src/main/java/com/digitalbooking/backend/service/CategoryService.java
+++ backend/src/main/java/com/digitalbooking/backend/service/CategoryService.java
@@ -20,33 +20,20 @@ public class CategoryService {
         this.categoryRepository = categoryRepository;
     }
 
-    public Category save(Category category) {
-        LOGGER.info("We have received your request. Please wait.");
-        categoryRepository.save(category);
-        LOGGER.info("Order completed. We are sending the result.");
-        return category;
-    }
-
-    public Optional<Category> findById(Long id) throws ResourceNotFoundException {
+    public Optional<Category> findBy(Long id) throws ResourceNotFoundException {
         LOGGER.info("We have received your request. Please wait.");
         return categoryRepository.findById(id);
     }
 
+    public Optional<Category> findByTitle(String title) throws ResourceNotFoundException {
+        LOGGER.info("We have received your request. Please wait.");
+        return categoryRepository.findByTitle(title);
+    }
+
+
     public List<Category> findAll() throws ResourceNotFoundException {
         LOGGER.info("We have received your request. Please wait.");
         return categoryRepository.findAll();
-    }
-
-    public void update(Category category) throws BadRequestException, ResourceNotFoundException {
-        LOGGER.info("We have received your request. Please wait.");
-        categoryRepository.save(category);
-        LOGGER.info("Order completed. We are sending the result.");
-    }
-
-    public void deleteById(Long id) throws ResourceNotFoundException {
-        LOGGER.info("We have received your request. Please wait.");
-        categoryRepository.deleteById(id);
-        LOGGER.info("Order completed. We are sending the result.");
     }
 
 }
