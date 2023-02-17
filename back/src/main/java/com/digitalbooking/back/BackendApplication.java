75dfe42c697f2f71db666d95e868c6a8eadf69e7
florencialecha <flecharegueira@gmail.com>
2023-02-16 21:03:16 -0300

    Cambiar nombre proyecto a backend

--- back/src/main/java/com/digitalbooking/back/BackendApplication.java
+++ back/src/main/java/com/digitalbooking/back/BackendApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackApplication {
+public class BackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackApplication.class, args);
+		SpringApplication.run(BackendApplication.class, args);
 	}
 
 }
