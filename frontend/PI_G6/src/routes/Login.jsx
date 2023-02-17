a2732514acf78024372e173360e0661f3f2f4b20
florencialecha <flecharegueira@gmail.com>
2023-02-16 21:40:56 -0300

    Eliminar carpeta front, limpiar la rama

--- frontend/PI_G6/src/routes/Login.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Login = () => {
-  return (
-    <div>You are in the Login form</div>
-  )
-}
-
-export default Login
--- frontend/PI_G6/src/routes/Register.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Register = () => {
-  return (
-    <div>You are in the Register form</div>
-  )
-}
-
-export default Register
