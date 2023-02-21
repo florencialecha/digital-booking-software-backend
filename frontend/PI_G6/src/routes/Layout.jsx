a446c6caa5481b6ff24fc9c8eb1eecaad53519db
rockyz7 <roocky_16@hotmail.com>
2023-02-21 10:43:59 -0300

    Footer responsive con css, creación de componente para redes sociales

--- frontend/PI_G6/src/routes/Layout.jsx
+++ frontend/PI_G6/src/routes/Layout.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Outlet } from "react-router";
 import Header from "../components/header/Header";
-import Footer from "../components/Footer";
+import Footer from "../components/footer/Footer";
 
 const Layout = () => {
   return (
