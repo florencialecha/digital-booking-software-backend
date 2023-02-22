49426f616f9e2c84a2c258cb6659318ebbd91752
rockyz7 <roocky_16@hotmail.com>
2023-02-22 19:34:20 -0300

    Agregado de link a los iconos de redes sociales

--- frontend/PI_G6/src/components/socials/Social.jsx
+++ frontend/PI_G6/src/components/socials/Social.jsx
@@ -7,14 +7,23 @@ import {
   faTwitter,
   faInstagram,
 } from "@fortawesome/free-brands-svg-icons";
+import { Link } from "react-router-dom";
 
 const Social = () => {
   return (
     <div className="socials">
-      <FontAwesomeIcon icon={faFacebook} size="xl" />
-      <FontAwesomeIcon icon={faLinkedinIn} size="xl" />
-      <FontAwesomeIcon icon={faTwitter} size="xl" />
-      <FontAwesomeIcon icon={faInstagram} size="xl" />
+      <Link to="https://es-la.facebook.com/" target="_blank">
+        <FontAwesomeIcon icon={faFacebook} size="xl" />
+      </Link>
+      <Link to="https://www.linkedin.com/" target="_blank">
+        <FontAwesomeIcon icon={faLinkedinIn} size="xl" />
+      </Link>
+      <Link to="https://twitter.com/" target="_blank">
+        <FontAwesomeIcon icon={faTwitter} size="xl" />
+      </Link>
+      <Link to="https://www.instagram.com/" target="_blank">
+        <FontAwesomeIcon icon={faInstagram} size="xl" />
+      </Link>
     </div>
   );
 };
