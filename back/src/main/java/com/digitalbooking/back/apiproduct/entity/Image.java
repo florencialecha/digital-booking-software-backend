612f2b624e3569567735a644569f156abc18b6cd
Florencia Lecha <flecharegueira@gmail.com>
2023-03-01 18:11:21 -0300

    Añadir clase image

---
+++ back/src/main/java/com/digitalbooking/back/apiproduct/entity/Image.java
@@ -0,0 +1,29 @@
+package com.digitalbooking.back.apiproduct.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "image")
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+
+public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "imageUrl")
+    private String imageUrl;
+
+}
--- back/src/main/java/com/digitalbooking/back/apiproduct/entity/Product.java
+++ back/src/main/java/com/digitalbooking/back/apiproduct/entity/Product.java
@@ -1,9 +1,12 @@
 package com.digitalbooking.back.apiproduct.entity;
 
+import java.awt.*;
+
 public class Product {
 
     private Long id;
     private String title;
     private String category;
     private Adress adress;
+    private Image
 }
