44fa1ef730a98e10db348fbb306ff033a783f99a
Florencia Lecha <flecharegueira@gmail.com>
2023-03-01 18:21:36 -0300

    Añadir atributos producto

--- back/src/main/java/com/digitalbooking/back/apiproduct/entity/Product.java
+++ back/src/main/java/com/digitalbooking/back/apiproduct/entity/Product.java
@@ -1,12 +1,45 @@
 package com.digitalbooking.back.apiproduct.entity;
 
-import java.awt.*;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Table(name = "product")
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 
 public class Product {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "title")
     private String title;
+    @Column(name = "category")
     private String category;
+
+    @Column(name = "adress")
     private Adress adress;
-    private Image
+
+    @Column(name = "image")
+    private List<Image> imageList;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "availability")
+    private String availability;
+
+    @Column(name = "policies")
+    private String policies;
+    
 }
