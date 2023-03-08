289723f4e604d3a7ce59f3582720bb23c41e1d8a
Florencia Lecha <flecharegueira@gmail.com>
2023-03-08 14:06:00 -0300

    Añadir tabla feature, policy y sus relaciones correspondientes

--- back/src/main/java/com/digitalbooking/back/apiProduct/entity/Policy.java
+++ back/src/main/java/com/digitalbooking/back/apiProduct/entity/Policy.java
@@ -1,4 +1,5 @@
 package com.digitalbooking.back.apiProduct.entity;
+
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -6,27 +7,27 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name = "IMAGE")
+@Table(name = "POLICY")
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 
-public class Image {
+public class Policy {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "IMAGE_ID")
+    @Column(name = "POLICY_ID")
     private Long id;
+    @Column(name = "RULES_DESCRIPTION")
+    private String rulesDescription;
+    @Column(name = "SECURITY_DESCRIPTION")
+    private String securityDescription;
+    @Column(name = "CANCELLATION_DESCRIPTION")
+    private String cancellationDescription;
 
-    @Column(name = "TITLE")
-    private String title;
-
-    @Column(name = "IMAGEURL")
-    private String imageUrl;
-
-    @ManyToOne
+    @OneToOne
     @JoinColumn(name = "PRODUCT_ID")
     private Product product;
 
