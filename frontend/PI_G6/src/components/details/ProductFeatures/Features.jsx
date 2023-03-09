60fa001c12d077e182a59d0373a137e8615b00b6
rockyz7 <roocky_16@hotmail.com>
2023-03-09 20:57:17 -0300

    Estilo correcto de iconos y policies

--- frontend/PI_G6/src/components/details/ProductFeatures/Features.jsx
+++ frontend/PI_G6/src/components/details/ProductFeatures/Features.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import './Features.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React from "react";
+import "./Features.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faKitchenSet,
   faPersonSwimming,
@@ -8,41 +8,63 @@ import {
   faSnowflake,
   faPaw,
   faCar,
-  faTv
-} from '@fortawesome/free-solid-svg-icons'
+  faTv,
+} from "@fortawesome/free-solid-svg-icons";
+import { useLocation } from "react-router";
 
 const icon = [
   {
-    title: 'Cocina',
-    iconName: faKitchenSet
+    title: "Cocina",
+    iconName: faKitchenSet,
   },
-  { title: 'WiFi', iconName: faWifi },
-  { title: 'Pileta', iconName: faPersonSwimming },
-  { title: 'Aire acondicionado', iconName: faSnowflake },
-  { title: 'Apto mascotas', iconName: faPaw },
-  { title: 'Estacionamiento gratuito', iconName: faCar },
-  { title: 'Televisor', iconName: faTv }
-]
+  { title: "WiFi", iconName: faWifi },
+  { title: "Pileta", iconName: faPersonSwimming },
+  { title: "Aire acondicionado", iconName: faSnowflake },
+  { title: "Apto mascotas", iconName: faPaw },
+  { title: "Estacionamiento gratuito", iconName: faCar },
+  { title: "Televisor", iconName: faTv },
+];
 
 const Features = ({ specs }) => {
+  const location = useLocation();
+
   return (
-    <div className="features">
-      <div className="features-body">
+    <div className={location.pathname !== "/" ? "features-home" : ""}>
+      <div
+        className={
+          location.pathname !== "/" ? "features-body" : "features-body-home"
+        }
+      >
         {specs.map((item) => {
-          let iconos = icon.find((i) => i.title.toLowerCase() === item.title.toLowerCase());
+          let iconos = icon.find(
+            (i) => i.title.toLowerCase() === item.title.toLowerCase()
+          );
           return (
-            <div key={item.title} className="feature-icons">
+            <div
+              key={item.title}
+              className={
+                location.pathname !== "/"
+                  ? "feature-icons"
+                  : "feature-icons-home"
+              }
+            >
               <FontAwesomeIcon
                 icon={iconos?.iconName}
                 size="lg"
               ></FontAwesomeIcon>
-              <span>{iconos?.title}</span>
+              <span
+                className={
+                  location.pathname === "/" ? "hide-icon-title" : "icon-name"
+                }
+              >
+                {iconos?.title}
+              </span>
             </div>
-          )
+          );
         })}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Features
+export default Features;
