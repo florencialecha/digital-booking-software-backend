85aac8aca104038d4bc25eb98ba283f9bb58d384
Florencia Lecha <flecharegueira@gmail.com>
2023-03-15 15:06:38 -0300

    Añadir logger y exceptions al método CreateCategoryPostController

--- back/src/main/java/com/digitalbooking/back/management/categories/controller/CreateCategoryPostController.java
+++ back/src/main/java/com/digitalbooking/back/management/categories/controller/CreateCategoryPostController.java
@@ -3,13 +3,16 @@ package com.digitalbooking.back.management.categories.controller;
 import com.digitalbooking.back.management.categories.entity.Category;
 import com.digitalbooking.back.management.categories.exception.BadRequestException;
 import com.digitalbooking.back.management.categories.service.CategoryService;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/category")
 @CrossOrigin("*")
-
+@Log4j2
 
 public class CreateCategoryPostController {
 
@@ -17,8 +20,14 @@ public class CreateCategoryPostController {
     private CategoryService categoryService;
 
     @PostMapping
-    public void handle(@RequestBody Category category) throws BadRequestException {
-        categoryService.create(category);
+    public void handle(@RequestBody Category category) {
+        try {
+            categoryService.create(category);
+            log.info("Category created successfully");
+        } catch (BadRequestException e) {
+            log.error("Bad Request Exception: {}", e.getMessage());
+        } catch (Exception e) {
+            log.error("Internal Server Error: {}", e.getMessage());
+        }
     }
-    
 }
