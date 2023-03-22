3325e40cfd71657deb84fbcfc698faaae53c94d4
damiandellacqua <d.dellacquaruiz@gmail.com>
2023-03-22 13:08:42 -0300

    actualizo el front con los cambios en los llamados de las apis

--- frontend/PI_G6/src/components/reserve/ArrivalInfo.jsx
+++ frontend/PI_G6/src/components/reserve/ArrivalInfo.jsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCircleCheck } from '@fortawesome/free-regular-svg-icons'
-import HourSelector from './hourSelector'
+import HourSelector from './HourSelector.jsx'
 
 const ArrivalInfo = ({ styles }) => {
   return (
