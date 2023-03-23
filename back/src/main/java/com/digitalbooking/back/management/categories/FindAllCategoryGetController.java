310bdcfccb0618f8d8b1b582f3ba2dcde51be420
Florencia Lecha <flecharegueira@gmail.com>
2023-03-23 13:51:21 -0300

    Corregir endpoint category

--- back/src/main/java/com/digitalbooking/back/management/categories/FindAllCategoryGetController.java
+++ back/src/main/java/com/digitalbooking/back/management/categories/FindAllCategoryGetController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/casas")
+@RequestMapping("/category")
 @CrossOrigin("*")
 @Log4j2
 
