b751b00c5cf57438b1f5494eb6725134b8f59532
Florencia Lecha <flecharegueira@gmail.com>
2023-03-24 17:17:45 -0300

    Métodos de crear reserva, usuario, rol

--- back/src/main/java/com/digitalbooking/back/bookStayApp/reserves/FindReservesByProductIdGetController.java
+++ back/src/main/java/com/digitalbooking/back/bookStayApp/reserves/FindReservesByProductIdGetController.java
@@ -1,29 +1,29 @@
-//package com.digitalbooking.back.bookStayApp.reserves;
-//
-//import com.digitalbooking.back.bookStayApp.products.exception.ResourceNotFoundException;
-//import lombok.extern.log4j.Log4j2;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.List;
-//@RestController
-//@RequestMapping("/reserve")
-//@CrossOrigin("*")
-//@Log4j2
-//
-//public class FindReservesByProductIdGetController {
-//
-//    @Autowired
-//    private FindReservesByProductIdService findReservesByProductIdService;
-//
-//    @GetMapping("/byProductId/{idProduct}")
-//    public ResponseEntity<List<Reserve>> handle(@PathVariable Long idProduct) throws ResourceNotFoundException {
-//        List<Reserve> reserves = findReservesByProductIdService.handle(idProduct);
-//        if (reserves.isEmpty()) {
-//            throw new ResourceNotFoundException("Can't find a reserves for this product.");
-//        }
-//        return ResponseEntity.ok(reserves);
-//    }
-//
-//}
+package com.digitalbooking.back.bookStayApp.reserves;
+
+import com.digitalbooking.back.bookStayApp.products.exception.ResourceNotFoundException;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@RestController
+@RequestMapping("/reserve")
+@CrossOrigin("*")
+@Log4j2
+
+public class FindReservesByProductIdGetController {
+
+    @Autowired
+    private FindReservesByProductIdService findReservesByProductIdService;
+
+    @GetMapping("/byProductId/{idProduct}")
+    public ResponseEntity<List<Reserve>> handle(@PathVariable Long idProduct) throws ResourceNotFoundException {
+        List<Reserve> reserves = findReservesByProductIdService.handle(idProduct);
+        if (reserves.isEmpty()) {
+            throw new ResourceNotFoundException("Can't find a reserves for this product.");
+        }
+        return ResponseEntity.ok(reserves);
+    }
+
+}
--- back/src/main/java/com/digitalbooking/back/bookStayApp/reserves/ReserveToCreateDTO.java
+++ back/src/main/java/com/digitalbooking/back/bookStayApp/reserves/ReserveToCreateDTO.java
@@ -1,21 +1,21 @@
-//package com.digitalbooking.back.bookStayApp.reserves;
-//
-//import lombok.Getter;
-//import lombok.NoArgsConstructor;
-//import lombok.Setter;
-//
-//import java.time.LocalDate;
-//import java.time.LocalTime;
-//
-//@Getter
-//@Setter
-//@NoArgsConstructor
-//public class ReserveToCreateDTO {
-//
-//    private LocalTime startTime;
-//    private LocalDate checkIn;
-//    private LocalDate checkOut;
-//    private Long product;
-//    private Long user;
-//
-//}
+package com.digitalbooking.back.bookStayApp.reserves;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ReserveToCreateDTO {
+
+    private LocalTime startTime;
+    private LocalDate checkIn;
+    private LocalDate checkOut;
+    private Long product;
+    private Long user;
+
+}
