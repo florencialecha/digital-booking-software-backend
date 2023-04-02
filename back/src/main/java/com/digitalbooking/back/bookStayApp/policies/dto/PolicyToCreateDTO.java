b2c1985a390989231453a5b21bc62b497282ecd1
Florencia Lecha <flecharegueira@gmail.com>
2023-04-02 19:53:43 -0300

    Corregir método find product by id

--- back/src/main/java/com/digitalbooking/back/bookStayApp/policies/dto/PolicyToCreateDTO.java
+++ back/src/main/java/com/digitalbooking/back/bookStayApp/policies/dto/PolicyToCreateDTO.java
@@ -1,13 +1,12 @@
 package com.digitalbooking.back.bookStayApp.policies.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@AllArgsConstructor
+@NoArgsConstructor
 public class PolicyToCreateDTO {
     private String rules;
     private String security;
--- back/src/main/java/com/digitalbooking/back/management/categories/controller/FindAllCategoriesGetController.java
+++ back/src/main/java/com/digitalbooking/back/management/categories/controller/FindAllCategoriesGetController.java
@@ -19,7 +19,7 @@ import java.util.stream.Collectors;
 @RequestMapping("/category")
 @CrossOrigin("*")
 @Log4j2
-public class FindAllCategoryGetController {
+public class FindAllCategoriesGetController {
 
     @Autowired
     private FindAllCategoryService findAllCategoryService;
