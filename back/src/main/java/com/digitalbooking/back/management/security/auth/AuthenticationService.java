2bb6308106e3f557fa486cddaf980bb1e746415b
Florencia Lecha <flecharegueira@gmail.com>
2023-04-03 17:09:34 -0300

    Autenticar identidad con token funciona

--- back/src/main/java/com/digitalbooking/back/management/security/auth/AuthenticationService.java
+++ back/src/main/java/com/digitalbooking/back/management/security/auth/AuthenticationService.java
@@ -8,7 +8,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
