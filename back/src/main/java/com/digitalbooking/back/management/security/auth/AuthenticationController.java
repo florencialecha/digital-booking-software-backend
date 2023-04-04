bdb51648582f98c41d1729856dd2e62e37bf2376
Florencia Lecha <flecharegueira@gmail.com>
2023-04-03 21:02:22 -0300

    Modificar cross auth controller

--- back/src/main/java/com/digitalbooking/back/management/security/auth/AuthenticationController.java
+++ back/src/main/java/com/digitalbooking/back/management/security/auth/AuthenticationController.java
@@ -4,15 +4,14 @@ import lombok.RequiredArgsConstructor;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 @Log4j2
+@CrossOrigin("*")
+
 public class AuthenticationController {
 
     private final AuthenticationService authenticationService;
