92f998c05381d900f823f064b114483e097cf565
Florencia Lecha <flecharegueira@gmail.com>
2023-04-04 14:11:22 -0300

    Modificar método crear reserva para que reciba el usuario via token

--- back/src/main/java/com/digitalbooking/back/bookStayApp/reserves/controller/CreateReservePostController.java
+++ back/src/main/java/com/digitalbooking/back/bookStayApp/reserves/controller/CreateReservePostController.java
@@ -16,7 +16,6 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/reserve")
