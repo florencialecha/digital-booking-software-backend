b2ea9307eba24d7b2a6662ab55a4987dda005640
Florencia Lecha <flecharegueira@gmail.com>
2023-04-03 22:19:58 -0300

    Añadir status 201 al crear usuario y validaciones

--- back/src/main/java/com/digitalbooking/back/management/security/auth/AuthenticationController.java
+++ back/src/main/java/com/digitalbooking/back/management/security/auth/AuthenticationController.java
@@ -1,8 +1,11 @@
 package com.digitalbooking.back.management.security.auth;
 
+import io.micrometer.common.util.StringUtils;
 import lombok.RequiredArgsConstructor;
 
 import lombok.extern.log4j.Log4j2;
+import org.apache.commons.validator.routines.EmailValidator;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,9 +19,26 @@ public class AuthenticationController {
 
     private final AuthenticationService authenticationService;
     @PostMapping("/register")
-    public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request) {
+    public ResponseEntity<?> register(@RequestBody RegisterRequest request) {
         log.info("Request received on AuthenticationController");
-        return ResponseEntity.ok(authenticationService.register(request));
+
+        //Validating user data
+        if (
+                StringUtils.isBlank(request.getFirstname()) ||
+                StringUtils.isBlank(request.getLastName()) ||
+                StringUtils.isBlank(request.getUsername()) ||
+                StringUtils.isBlank(request.getEmail()) ||
+                StringUtils.isBlank(request.getPassword())) {
+            return ResponseEntity.badRequest().body("All fields are required");
+        }
+        if (!EmailValidator.getInstance().isValid(request.getEmail())) {
+            return ResponseEntity.badRequest().body("Invalid email address");
+        }
+        if (request.getPassword().length() < 6) {
+            return ResponseEntity.badRequest().body("Password must be at least 6 characters");
+        }
+        AuthenticationResponse response = authenticationService.register(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
     //todo: cambiar a endpoint login
